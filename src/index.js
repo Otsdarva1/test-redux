afa9485207e9e54e066f4ad16d49da4bacb854be
Otsdarva1 <yamazaki192748@gmail.com>
2019-11-20 23:05:22 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "PLUS_ONE":
+      return state + 1;
+    case "MINUS_ONE":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "PLUS_ONE" });
+store.dispatch({ type: "PLUS_ONE" });
+store.dispatch({ type: "PLUS_ONE" });
+store.dispatch({ type: "MINUS_ONE" });
